f1153d026894208da0e1f747d5310e3b27fa7bf2
Jim Norman <jim@civitech.io>
2025-06-05 20:07:56 -0500

    initial version

---
+++ Parentheses/ParenthesesValidator.cs
@@ -0,0 +1,28 @@
+namespace Parentheses;
+
+public static class ParenthesesValidator
+{
+    public static bool AreParenthesesValid(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return true;
+        }
+
+        int count = 0;
+
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                count++;
+            }
+            else if (c == ')')
+            {
+                count--;
+            }
+        }
+
+        return count == 0;
+    }
+}
